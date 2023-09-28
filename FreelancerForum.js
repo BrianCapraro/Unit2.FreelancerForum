5eff58ba54a4889be82c85081eafe8bc11a72829
Brian Capraro <Briancapraro0228@gmail.com>
2023-09-28 15:21:10 -0400

    Added arrays for name, occupation, and price in js.

---
+++ FreelancerForum.js
@@ -0,0 +1,20 @@
+const freelancers = [
+    { name: "Dr. Slice", price: 30, occupation: "programmer" },
+    { name: "Dr. Pressure", price: 75, occupation: "contractor" },
+    { name: "Prof. Possibility", price: 50, occupation: "teacher" },
+    { name: "Prof. Prism", price: 61, occupation: "contractor" },
+    { name: "Dr. Impulse", price: 44, occupation: "driving instructor" },
+    { name: "Prof. Spark", price: 83, occupation: "programmer" },
+    { name: "Dr. Wire", price: 57, occupation: "electrician" },
+    { name: "Prof. Goose", price: 72, occupation: "pilot" },
+  ];
+
+  const name = ["Mr. Jimbob", "Ms. Sherbert", "Dr. Zarovich", "Kremmy Lops"]
+
+  const price = [23, 43, 55, 99, 81, 76]
+
+  const occupation = ["programmer", "contractor", "electritian", "pilot", "teacher", "driving instructor"]
+
+
+
+  
